5dbcc7aa57cea38fb863b95b2b604cf78f45fb85
Klimanova Evgenia <mgenya22@gmail.com>
2023-11-11 14:32:15 +0300

    Add 9 step.

--- app/src/main/java/hexlet/code/formatters/PlainFormatter.java
+++ app/src/main/java/hexlet/code/formatters/PlainFormatter.java
@@ -4,6 +4,42 @@ import java.util.Map;
 
 public class PlainFormatter {
     public static String format(Map<String, Map<String, Object>> formatDiffList) {
-        return null;
+        StringBuilder result = new StringBuilder();
+        for (Map.Entry<String, Map<String, Object>> f : formatDiffList.entrySet()) {
+            if (f.getValue().containsKey("  ")) {
+                continue;
+            }
+            result.append("Property '");
+            result.append(f.getKey());
+            if (f.getValue().containsKey("+ ") && f.getValue().containsKey("- ")) {
+                result.append("' was updated. From ");
+                result.append(resultValue(f.getValue().get("- ")));
+                result.append(" to ");
+                result.append(resultValue(f.getValue().get("+ ")));
+                result.append("\n");
+            } else if (f.getValue().containsKey("+ ")) {
+                result.append("' was added with value: ");
+                result.append(resultValue(f.getValue().get("+ ")));
+                result.append("\n");
+            } else if (f.getValue().containsKey("- ")) {
+                result.append("' was removed");
+                result.append("\n");
+            }
+        }
+        return result.toString();
+    }
+
+    private static String resultValue(Object kv) {
+        if (kv == null) {
+            return "null";
+        }
+        if (kv instanceof String) {
+            return "'" + kv + "'";
+        }
+        if (kv instanceof Integer || kv instanceof Boolean) {
+            return kv.toString();
+        } else {
+            return "[complex value]";
+        }
     }
 }
--- app/src/test/java/hexlet/code/DifferTest.java
+++ app/src/test/java/hexlet/code/DifferTest.java
@@ -8,6 +8,7 @@ import java.io.IOException;
 class DifferTest {
 
     public static final String STYLISH_FORMAT = "stylish";
+    public static final String PLAIN_FORMAT = "plain";
 
     @Test
     void generateShouldCorrectWorkWihtBothFileJson() throws IOException {
@@ -170,6 +171,86 @@ class DifferTest {
                       setting3: none
                 }""";
         String actual = Differ.generate(path1, path2, STYLISH_FORMAT);
+        Assertions.assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    void generateShouldCorrectWorkWihtBothFileJsonPlainFormat() throws IOException {
+        String path1 = "./src/test/resources/json/file1.json";
+        String path2 = "./src/test/resources/json/file2.json";
+        String expected = """
+                Property 'chars2' was updated. From [complex value] to false
+                Property 'checked' was updated. From false to true
+                Property 'default' was updated. From null to [complex value]
+                Property 'id' was updated. From 45 to null
+                Property 'key1' was removed
+                Property 'key2' was added with value: 'value2'
+                Property 'numbers2' was updated. From [complex value] to [complex value]
+                Property 'numbers3' was removed
+                Property 'numbers4' was added with value: [complex value]
+                Property 'obj1' was added with value: [complex value]
+                Property 'setting1' was updated. From 'Some value' to 'Another value'
+                Property 'setting2' was updated. From 200 to 300
+                Property 'setting3' was updated. From true to 'none'
+                """;
+        String actual = Differ.generate(path1, path2, PLAIN_FORMAT);
+        Assertions.assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    void generateShouldCorrectWorkWithSameFileYmlPlainFormat() throws IOException {
+        String path1 = "./src/test/resources/yaml/file2.yaml";
+        String path2 = "./src/test/resources/yaml/file2.yaml";
+        String expected = "";
+        String actual = Differ.generate(path1, path2, PLAIN_FORMAT);
+        Assertions.assertThat(actual).isEqualTo(expected);
+    }
+
+
+
+    @Test
+    void generateShouldCorrectWorkWihtBothFileYmlPlainFormat() throws IOException {
+        String path1 = "./src/test/resources/yaml/file1.yml";
+        String path2 = "./src/test/resources/yaml/file2.yaml";
+        String expected = """
+                Property 'chars2' was updated. From [complex value] to false
+                Property 'checked' was updated. From false to true
+                Property 'default' was updated. From null to [complex value]
+                Property 'id' was updated. From 45 to null
+                Property 'key1' was removed
+                Property 'key2' was added with value: 'value2'
+                Property 'numbers2' was updated. From [complex value] to [complex value]
+                Property 'numbers3' was removed
+                Property 'numbers4' was added with value: [complex value]
+                Property 'obj1' was added with value: [complex value]
+                Property 'setting1' was updated. From 'Some value' to 'Another value'
+                Property 'setting2' was updated. From 200 to 300
+                Property 'setting3' was updated. From true to 'none'
+                """;
+        String actual = Differ.generate(path1, path2, PLAIN_FORMAT);
+        Assertions.assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    void generateShouldCorrectWorkWithOneEmptyFileYmlPlainFormat() throws IOException {
+        String path1 = "./src/test/resources/yaml/file0.yaml";
+        String path2 = "./src/test/resources/yaml/file2.yaml";
+        String expected = """
+                Property 'chars1' was added with value: [complex value]
+                Property 'chars2' was added with value: false
+                Property 'checked' was added with value: true
+                Property 'default' was added with value: [complex value]
+                Property 'id' was added with value: null
+                Property 'key2' was added with value: 'value2'
+                Property 'numbers1' was added with value: [complex value]
+                Property 'numbers2' was added with value: [complex value]
+                Property 'numbers4' was added with value: [complex value]
+                Property 'obj1' was added with value: [complex value]
+                Property 'setting1' was added with value: 'Another value'
+                Property 'setting2' was added with value: 300
+                Property 'setting3' was added with value: 'none'
+                """;
+        String actual = Differ.generate(path1, path2, PLAIN_FORMAT);
         Assertions.assertThat(actual).isEqualTo(expected);
     }
 
